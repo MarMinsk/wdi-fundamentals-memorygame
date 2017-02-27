c4017a68f0bd114fd4cf72f4804c191637f4287d
Marisa Wheatley <Marisa@Marisas-MacBook-Pro.local>
2017-02-27 09:51:29 +0000

    updated game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,7 +1,13 @@
-console.log("JS file is connected to HTML! Woo!")
+// console.log("JS file is connected to HTML! Woo!") 
 
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+if (cardThree === cardFour) {
+  alert("You have a match!");
+}
+else {
+  alert("Sorry, try again.");
+}
