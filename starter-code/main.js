a3ea6fec92b597a98041bfb39078268b94ccdbc3
Marisa Wheatley <Marisa@Marisas-MacBook-Pro.local>
2017-03-09 10:55:16 +0000

    final assignment code

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,16 +1,16 @@
 // console.log("JS file is connected to HTML! Woo!") 
 
-var cardOne = "queen";
+/* var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
-var cardFour = "king";
+var cardFour = "king"; 
 
-/* if (cardThree === cardFour) {
+if (cardThree === cardFour) {
   alert("You have a match!");
 }
 else {
   alert("Sorry, try again.");
-} */
+} 
 
 
 var createCards = function() {
@@ -21,6 +21,51 @@ var createCards = function() {
     newCard.className = 'card';
     gameBoard.appendChild(newCard);
   }
+} */
+
+
+var cards = ['queen', 'queen', 'king', 'king']; // step 1
+var cardsInPlay = []; // step 2
+var board = document.getElementById('game-board');
+
+function createBoard() {  //step 3 
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('div'); 
+    cardElement.className = 'card';
+    cardElement.setAttribute('data-card', cards[i]); //step 3
+    cardElement.addEventListener('click', isTwoCards); //step 4
+    board.appendChild(cardElement);
+  }
 }
 
-createCards();
+function isMatch(cards){ // step 5 
+  if (cards[0].getAttribute('data-card') === cards[1].getAttribute('data-card')) {
+    alert("You have a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
+
+function isTwoCards() { // step 6 - connected to step 4
+  var cardClicked = this;  // var that links 'this' to 'data-card' attribute
+  var typeOfCardClicked = cardClicked.getAttribute('data-card'); // created var for ease of reading
+  cardsInPlay.push(cardClicked); 
+  if (typeOfCardClicked === 'king') {
+    cardClicked.innerHTML = '<img src="my-king.png" alt="King of Hearts" />'; // step 7 and 8
+  } else {
+    cardClicked.innerHTML = '<img src="my-queen.png" alt="Queen of Hearts" />'; 
+  }
+
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    setTimeout(function () { // step 9 - setting a timeout for 5 seconds to remove cards from array
+      for (var i = 0; i < cardsInPlay.length; i++) {
+        cardsInPlay[i].innerHTML = '';
+      }
+      cardsInPlay=[];
+    }, 5000)
+  }
+}
+
+createBoard();
+
